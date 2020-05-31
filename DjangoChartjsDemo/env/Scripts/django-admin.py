8e28a96fcd1afa2dbcc9be64fd0721ddf26c123f
fscheu <34041934+fscheu@users.noreply.github.com>
2020-05-31 12:54:31 -0300

    Add project files.

---
+++ DjangoChartjsDemo/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\baiscf\source\repos\DjangoChartjsDemo\DjangoChartjsDemo\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
